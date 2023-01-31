458b33bf6a47bd564c276a7cb097057a849e448b
prashant-learning <prashantkumar02999@gmail.com>
2023-01-31 09:21:32 +0530

    initial commit for email service

---
+++ src/main/java/com/wipro/email/service/SendEmail.java
@@ -0,0 +1,74 @@
+package com.wipro.email.service;
+
+import javax.mail.*;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.util.Properties;
+
+public class SendEmail {
+
+    public static void main(String[] args) {
+
+        for (int i =0 ; i < args.length; i++){
+            System.out.println(args[i]);
+        }
+
+        // Recipient's email ID needs to be mentioned.
+        String to = args[0];
+
+        // Sender's email ID needs to be mentioned
+        String from = args[1];
+
+        // Assuming you are sending email from through gmails smtp
+        String host = "smtp.gmail.com";
+
+        // Get system properties
+        Properties properties = System.getProperties();
+
+        // Setup mail server
+        properties.put("mail.smtp.host", host);
+        properties.put("mail.smtp.port", "465");
+        properties.put("mail.smtp.ssl.enable", "true");
+        properties.put("mail.smtp.auth", "true");
+
+
+
+        // Get the Session object.// and pass username and password
+        Session session = Session.getInstance(properties, new javax.mail.Authenticator() {
+
+            protected PasswordAuthentication getPasswordAuthentication() {
+
+                return new PasswordAuthentication(from, args[4]);
+
+            }
+
+        });
+
+        // Used to debug SMTP issues
+        session.setDebug(true);
+
+        try {
+            // Create a default MimeMessage object.
+            MimeMessage message = new MimeMessage(session);
+
+            // Set From: header field of the header.
+            message.setFrom(new InternetAddress(from));
+
+            // Set To: header field of the header.
+            message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+
+            // Set Subject: header field
+            message.setSubject(args[2]);
+
+            // Now set the actual message
+            message.setText(args[3]);
+
+            System.out.println("sending...");
+            // Send message
+            Transport.send(message);
+            System.out.println("Sent message successfully....");
+        } catch (MessagingException mex) {
+            mex.printStackTrace();
+        }
+    }
+}
